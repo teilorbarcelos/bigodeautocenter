271c12b7ff9b6e5965a019773ba5c78f2e7f567f
Teilor Souza Barcelos <tsb-@live.com>
2021-11-10 14:53:04 -0300

    client and sale pages finisheds

--- pages/client/id/[id].tsx
+++ pages/client/id/[id].tsx
@@ -159,52 +159,56 @@ const Client: NextPage = () => {
 
             {/* SALES */}
 
-            <div className={styles.sales}>
-              <div className={styles.salesTitle}>
-                <h5>Vendas:</h5>
-              </div>
-              {sales.map((sale) => {
-                const stringDate = sale.createdAt.split('T')[0].split('-')
-                const date = `${stringDate[2]}/${stringDate[1]}/${stringDate[0]}`
-                return (
-                  <Link
-                    key={sale.id}
-                    href={`/sale/id/${sale.id}`}
-                  >
-                    <a>
-                      <div
-                        title="Ver detalhes"
-                        className={styles.sale}
-                      >
-                        <div className={styles.head}>
-                          <p>{date}</p>
-                          <p>Carro: {sale.car}</p>
-                        </div>
-                        <div className={styles.preview}>
-                          <div>
-                            <p>Placa:</p>
-                            <p>{sale.plate}</p>
-                          </div>
-                          <div>
-                            <p>Total R$:</p>
-                            <p>{sale.total}</p>
-                          </div>
-                          <div>
-                            <p>Status:</p>
-                            <p
-                              className={`${sale.paid && styles.paid} ${styles.status}`}
-                            >
-                              {sale.paid ? 'Pago' : 'Pendente'}
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </a>
-                  </Link>
-                )
-              })}
+            {
+              sales.length > 0 &&
 
-            </div>
+              <div className={styles.sales}>
+                <div className={styles.salesTitle}>
+                  <h5>Vendas:</h5>
+                </div>
+                {sales.map((sale) => {
+                  const stringDate = sale.createdAt.split('T')[0].split('-')
+                  const date = `${stringDate[2]}/${stringDate[1]}/${stringDate[0]}`
+                  return (
+                    <Link
+                      key={sale.id}
+                      href={`/sale/id/${sale.id}`}
+                    >
+                      <a>
+                        <div
+                          title="Ver detalhes"
+                          className={styles.sale}
+                        >
+                          <div className={styles.head}>
+                            <p>{date}</p>
+                            <p>Carro: {sale.car}</p>
+                          </div>
+                          <div className={styles.preview}>
+                            <div>
+                              <p>Placa:</p>
+                              <p>{sale.plate}</p>
+                            </div>
+                            <div>
+                              <p>Total R$:</p>
+                              <p>{sale.total}</p>
+                            </div>
+                            <div>
+                              <p>Status:</p>
+                              <p
+                                className={`${sale.paid && styles.paid} ${styles.status}`}
+                              >
+                                {sale.paid ? 'Pago' : 'Pendente'}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </a>
+                    </Link>
+                  )
+                })}
+
+              </div>
+            }
 
           </>
         }
